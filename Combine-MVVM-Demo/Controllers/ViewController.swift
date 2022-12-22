d5cb6c37e0870dbbde0fbec3120031e68c2d0f26
Anatoli Stepanuke <anatolistepanuke@yandex.by>
2022-12-22 16:20:37 +0300

    Initial develop branch

--- Combine-MVVM-Demo/Controllers/ViewController.swift
+++ Combine-MVVM-Demo/Controllers/ViewController.swift
@@ -1,8 +1,14 @@
 import UIKit
 
 final class ViewController: UIViewController {
+    // MARK: - Properties
+
+    // MARK: - Constants
+
+    // MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .blue
     }
+    // MARK: - Setups
 }
