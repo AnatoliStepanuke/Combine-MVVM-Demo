3a2e767adba5e36b3bc32536d610fe812107744e
Anatoli Stepanuke <anatolistepanuke@yandex.by>
2022-12-22 16:17:18 +0300

    Project setting

---
+++ Combine-MVVM-Demo/Delegates/SceneDelegate.swift
@@ -0,0 +1,15 @@
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+    // MARK: - Properties
+    var window: UIWindow?
+
+    // MARK: - UIScene
+    func scene(
+        _ scene: UIScene,
+        willConnectTo session: UISceneSession,
+        options connectionOptions: UIScene.ConnectionOptions
+    ) {
+//        guard let _ = (scene as? UIWindowScene) else { return }
+    }
+}
