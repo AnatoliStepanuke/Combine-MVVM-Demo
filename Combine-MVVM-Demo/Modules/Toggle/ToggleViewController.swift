3ca9a85fa263b549ad8086b045a7451c0c777a23
Anatoli Stepanuke <anatolistepanuke@yandex.by>
2023-01-18 14:50:51 +0300

    Initial new branch

---
+++ Combine-MVVM-Demo/Modules/Toggle/ToggleViewController.swift
@@ -0,0 +1,21 @@
+import UIKit
+import Combine
+
+final class ToggleViewController: UIViewController {
+    // MARK: - Outlets
+    @IBOutlet weak var toggleLabel: UILabel!
+
+    // MARK: - Constants
+    // MARK: - Properties
+    // MARK: - Lifecycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    // MARK: - Setups
+    // MARK: - Actions
+    @IBAction func toggleOne(_ sender: Any) { }
+    @IBAction func toggleTwo(_ sender: Any) { }
+    @IBAction func toggleThree(_ sender: Any) { }
+    @IBAction func toggleFour(_ sender: Any) { }
+    @IBAction func toggleFive(_ sender: Any) { }
+}
---
+++ Combine-MVVM-Demo/Modules/Toggle/ToggleViewModel.swift
@@ -0,0 +1,10 @@
+import Foundation
+import Combine
+
+final class ToggleViewModel {
+    // MARK: - Constants
+    // MARK: - Properties
+    // MARK: - Init
+    // MARK: - API
+    // MARK: - Setups
+}
