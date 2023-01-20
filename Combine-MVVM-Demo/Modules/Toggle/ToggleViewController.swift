833583df391101dd8f0362d878b3d3749812991e
Anatoli Stepanuke <anatolistepanuke@yandex.by>
2023-01-20 23:38:29 +0300

    Realization ToggleViewController and ToggleViewModel

--- Combine-MVVM-Demo/Modules/Toggle/ToggleViewController.swift
+++ Combine-MVVM-Demo/Modules/Toggle/ToggleViewController.swift
@@ -4,18 +4,61 @@ import Combine
 final class ToggleViewController: UIViewController {
     // MARK: - Outlets
     @IBOutlet weak var toggleLabel: UILabel!
+    @IBOutlet weak var toggleButtonOne: UISwitch!
+    @IBOutlet weak var toggleButtonTwo: UISwitch!
+    @IBOutlet weak var toggleButtonThree: UISwitch!
+    @IBOutlet weak var toggleButtonFour: UISwitch!
+    @IBOutlet weak var toggleButtonFive: UISwitch!
 
     // MARK: - Constants
+    private let toggleViewModel = ToggleViewModel()
+
     // MARK: - Properties
+    private var cancellables = Set<AnyCancellable>()
+
     // MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupBindings()
     }
+
     // MARK: - Setups
+    private func setupBindings() {
+        toggleViewModel.toggleSwitchLabel.sink(receiveValue: { [weak self] text in
+            self?.toggleLabel.text = text
+        })
+        .store(in: &cancellables)
+    }
+
     // MARK: - Actions
-    @IBAction func toggleOne(_ sender: Any) { }
-    @IBAction func toggleTwo(_ sender: Any) { }
-    @IBAction func toggleThree(_ sender: Any) { }
-    @IBAction func toggleFour(_ sender: Any) { }
-    @IBAction func toggleFive(_ sender: Any) { }
+    @IBAction func toggleOne(_ sender: Any) {
+        switch toggleButtonOne.isOn {
+        case true: toggleViewModel.toggleSwitchButtonIsEnabled.send()
+        case false: toggleViewModel.toggleSwitchButtonIsDisabled.send()
+        }
+    }
+    @IBAction func toggleTwo(_ sender: Any) {
+        switch toggleButtonTwo.isOn {
+        case true: toggleViewModel.toggleSwitchButtonIsEnabled.send()
+        case false: toggleViewModel.toggleSwitchButtonIsDisabled.send()
+        }
+    }
+    @IBAction func toggleThree(_ sender: Any) {
+        switch toggleButtonThree.isOn {
+        case true: toggleViewModel.toggleSwitchButtonIsEnabled.send()
+        case false: toggleViewModel.toggleSwitchButtonIsDisabled.send()
+        }
+    }
+    @IBAction func toggleFour(_ sender: Any) {
+        switch toggleButtonFour.isOn {
+        case true: toggleViewModel.toggleSwitchButtonIsEnabled.send()
+        case false: toggleViewModel.toggleSwitchButtonIsDisabled.send()
+        }
+    }
+    @IBAction func toggleFive(_ sender: Any) {
+        switch toggleButtonFive.isOn {
+        case true: toggleViewModel.toggleSwitchButtonIsEnabled.send()
+        case false: toggleViewModel.toggleSwitchButtonIsDisabled.send()
+        }
+    }
 }
--- Combine-MVVM-Demo/Modules/Toggle/ToggleViewModel.swift
+++ Combine-MVVM-Demo/Modules/Toggle/ToggleViewModel.swift
@@ -3,8 +3,50 @@ import Combine
 
 final class ToggleViewModel {
     // MARK: - Constants
+    private let toggles2: [Bool] = [true, true, true, true, true]
+
     // MARK: - Properties
+    private var cancellables = Set<AnyCancellable>()
+    private var toggles: [Bool] = []
+
+    private var toggleSwitchIsEnabled: Bool = false {
+        didSet {
+            toggles.append(toggleSwitchIsEnabled)
+        }
+    }
+
+    private var text = "" {
+        didSet {
+            toggleSwitchLabel.send("\(text)")
+        }
+    }
+
+    // MARK: - Input
+    let toggleSwitchButtonIsEnabled = PassthroughSubject<Void, Never>()
+    let toggleSwitchButtonIsDisabled = PassthroughSubject<Void, Never>()
+
+    // MARK: - Output
+    let toggleSwitchLabel = CurrentValueSubject<String, Never>("")
+    var toggleSwitchesIsEnabled = CurrentValueSubject<Bool, Never>(false)
+
     // MARK: - Init
+    init() {
+        toggleSwitchButtonIsEnabled.sink { [weak self] in
+            self?.toggleSwitchIsEnabled = true
+            if self?.toggles == self?.toggles2 {
+                self?.text = "All toggles are Enabled"
+            }
+        }
+        .store(in: &cancellables)
+
+        toggleSwitchButtonIsDisabled.sink { [weak self] in
+            self?.text = ""
+            self?.toggleSwitchIsEnabled = false
+            self?.toggles.removeLast(2)
+        }
+        .store(in: &cancellables)
+    }
+
     // MARK: - API
     // MARK: - Setups
 }
